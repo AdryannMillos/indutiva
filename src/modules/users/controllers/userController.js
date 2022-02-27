bd69a7fd28e76c1a957838381e47fdfb867a1b6f
Adryann Millos <adryann312@gmail.com>
2022-02-26 23:25:15 -0300

    Finished the CRUD and Login with jwt

--- src/modules/users/controllers/userController.js
+++ src/modules/users/controllers/userController.js
@@ -1,5 +1,7 @@
 const Models = require("../../../models/index");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 
 async function store(req, res) {
   const { userName, password, email, confirmPassword } = req.body;
@@ -47,26 +49,82 @@ async function update(req, res) {
       email: email,
       password: password,
     };
-    if (password !== confirmPassword) {
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
+    } else {
+      if (password !== confirmPassword) {
         res.status(401).json({ message: "Password does not match" });
       } else {
-        await bcrypt.hash(password, 10, (err, hash) => {
+        bcrypt.hash(password, 10, (err, hash) => {
           if (err) {
             res.status(500).json({ message: err.message });
           } else {
             updatedUser.password = hash;
-            Models.User.update(updatedUser);
-            return res.status(201).json({ message: "User Updated successfully"});
+            console.log(updatedUser.password);
+            Models.User.update(
+              { userName: updatedUser.userName, email: updatedUser.email, password:  updatedUser.password },
+              { where: { id: userId } }
+            );
+            return res
+              .status(200)
+              .json({ message: "User Updated successfully" });
           }
         });
       }
+    }
   } catch (err) {
     return res.status(500).json({ message: `Error:${err.message}` });
   }
 }
 
+async function destroy(req, res) {
+  try {
+    const userId = req.params.id;
+    const user = await Models.User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User Not Found" });
+    } else {
+      await Models.User.destroy({ where: { id: userId } });
+      return res.status(200).json({ message: "User Deleted successfully" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: `Error:${err.message}` });
+  }
+}
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const userWithEmail = await Models.User.findOne({ where: { email } }).catch(
+    (error) => {
+      res.send("Error: ", error.message);
+    }
+  );
+  if (!userWithEmail) {
+    return res.status(401).json({ message: "Email or password not found" });
+  }
+  if (!bcrypt.compareSync(password, userWithEmail.password)){
+    return res.status(401).json({ message: "Email or password not found" });
+  }
+
+  const jwtToken = jwt.sign(
+    {
+      id: userWithEmail.id,
+      userName: userWithEmail.userName,
+      email: userWithEmail.email,
+    },
+    process.env.JWT_SECRET || "somethingsecret",
+    {
+      expiresIn: "1h",
+    }
+  );
+
+  res.status(200).send({message: "Welcome Back!", token: jwtToken});
+};
+
 module.exports = {
   store,
   index,
   update,
+  destroy,
+  login
 };
--- src/modules/users/routes/api.user.router.js
+++ src/modules/users/routes/api.user.router.js
@@ -6,6 +6,9 @@ const userController = require("../controllers/userController");
 router.get("/", userController.index);
 router.post("/create", userController.store);
 router.put("/update/:id", userController.update);
+router.delete("/delete/:id", userController.destroy);
+
+router.post("/login", userController.login);
 
 
 
